0a11fd5ec8daa365d4f674df84f1e4faa62b9537
IshmamR <ishmam785@gmail.com>
2025-10-24 23:25:20 +0600

    dropped cjs

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,12 +1,16 @@
+import { $ } from "bun";
+
 /**
  * Build ESM and CJS from src/index.ts
  * - Externalize deps so your library isn't bundled with mongodb/bson
  * - Keep source maps optional (can enable if you want)
  */
 const entry = "src/index.ts";
-const external = ["mongodb", "bson"];
+const external = ["mongodb"];
 
 const footer = "\n// Made by promethewz to make mongodb a better place";
+
+await $`rm -rf dist`;
 
 await Bun.build({
   entrypoints: [entry],
@@ -15,19 +19,9 @@ await Bun.build({
   target: "node",
   sourcemap: "linked",
   naming: "[dir]/[name].js",
+  minify: true,
   external,
   footer,
 });
 
-await Bun.build({
-  entrypoints: [entry],
-  outdir: "dist",
-  format: "cjs",
-  target: "node",
-  sourcemap: "linked",
-  naming: "[dir]/[name].cjs",
-  external,
-  footer,
-});
-
-console.log("Built ESM: dist/index.js and CJS: dist/index.cjs");
+console.log("Built ESM: dist/index.js");
