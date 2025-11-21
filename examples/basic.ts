ea6b4ea5049ecd58eb7d3482f5ec564f4f6d2973
IshmamR <ishmam785@gmail.com>
2025-11-21 22:25:01 +0600

    basic example

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,6 +1,36 @@
-import { MongoClient } from "mongodb";
+/** biome-ignore-all lint/suspicious/noConsole: hmmm */
+import { M, mongster } from "../src";
 
-// biome-ignore lint/style/noNonNullAssertion: emni
-const client = await MongoClient.connect(process.env.MONGODB_URI!);
-console.log(client.options.dbName);
-await client.close();
+const userSchema = M.schema({
+  name: M.string(),
+  age: M.number(),
+  dob: M.date().optional(),
+  gender: M.boolean(),
+  role: M.string().enum(["admin", "janitor"] as const),
+  socials: M.array(
+    M.object({
+      host: M.string(),
+      link: M.string(),
+    }),
+  ),
+});
+
+type TUser = M.infer<typeof userSchema>;
+//    ^?
+type TUserCreate = M.inferInput<typeof userSchema>;
+//    ^?
+
+const UserModel = mongster.model("users", userSchema);
+
+await mongster.connect(process.env.DB_URL);
+
+const result = await UserModel.createOne({
+  name: "promethewz",
+  age: 18,
+  gender: true,
+  socials: [{ host: "github", link: "https://github.com/IshmamR" }],
+  role: "admin",
+  dob: new Date(),
+});
+
+console.log(result);
